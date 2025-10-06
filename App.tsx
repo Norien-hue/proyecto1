17b7e8ecec9cf9696bef10bf905d05f94c64bbff
Norien-euh <norah2006norah@outlook.com>
2025-10-06 18:53:19 +0200

    cabecera añadida

--- App.tsx
+++ App.tsx
@@ -1,12 +1,30 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { ScrollView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { Image } from 'expo-image'
 
 export default function App() {
+  const COLOR_FONDO="#121212"
+  const COLOR_TITULO="#ffdd99"
+  const COLOR_TEXTO_FOTO="#ffffff"
   return (
-    <View>
-      
+    <View style={[styles.contenedorPrincipal, {backgroundColor:COLOR_FONDO}]}>
+      <ScrollView>
+        <Image
+        source={require("./assets/granada_light.jpg")}
+        contentFit="contain"
+        style={styles.granada}
+        />
+      </ScrollView>
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  contenedorPrincipal:{
+    flex:1
+  },
+  granada:{
+    width:"100%",
+    height:250
+  }
+})

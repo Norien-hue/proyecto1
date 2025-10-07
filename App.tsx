dceae716a022cd6759a1fccb14a35b22402d945c
Norien-euh <norah2006norah@outlook.com>
2025-10-07 18:04:53 +0200

    mejores rutas terminadas

--- App.tsx
+++ App.tsx
@@ -1,11 +1,16 @@
 import { ScrollView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-import { Image } from 'expo-image'
+import { Image, ImageBackground } from 'expo-image'
+import { useFonts } from 'expo-font'
 
 export default function App() {
   const COLOR_FONDO="#121212"
   const COLOR_TITULO="#ffdd99"
   const COLOR_TEXTO_FOTO="#ffffff"
+  useFonts({
+    "bebasNeue": require("./assets/BebasNeue-Regular.ttf")
+  })
+
   return (
     <View style={[styles.contenedorPrincipal, {backgroundColor:COLOR_FONDO}]}>
       <ScrollView>
@@ -45,6 +50,30 @@ export default function App() {
             style={styles.fotoCarrusel}
           />
           </ScrollView>
+          <Text style={[styles.titulo, {color:COLOR_TITULO}]}>
+            Las mejores rutas
+          </Text>
+          <ImageBackground 
+            source={require("./assets/mejores1.jpg")}
+            contentFit='contain'
+            style={styles.fotoRuta}
+          >
+            <Text style={[styles.textoFoto, {color:COLOR_TEXTO_FOTO}]}>Albaicín</Text>
+          </ImageBackground>
+          <ImageBackground 
+            source={require("./assets/mejores2.jpg")}
+            contentFit='contain'
+            style={styles.fotoRuta}
+          >
+            <Text style={[styles.textoFoto, {color:COLOR_TEXTO_FOTO}]}>Sacromonte</Text>
+          </ImageBackground>
+          <ImageBackground 
+            source={require("./assets/mejores3.jpg")}
+            contentFit='contain'
+            style={styles.fotoRuta}
+          >
+            <Text style={[styles.textoFoto, {color:COLOR_TEXTO_FOTO}]}>El centro</Text>
+          </ImageBackground>
         </View>
       </ScrollView>
     </View>
@@ -73,5 +102,24 @@ const styles = StyleSheet.create({
     height:300,
     margin:10,
     borderRadius:10
-  }
+  },
+  fotoRuta:{
+    width:"100%",
+    height:200,
+    marginVertical: 5
+  },
+  textoFoto:{
+    margin:"auto",
+    fontSize:48,
+    fontFamily:"bebasNeue",
+    shadowColor: "#000",
+    shadowOffset: {
+	  width: 100,
+	  height: 25,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+
+    elevation: 5,
+    }
 })
